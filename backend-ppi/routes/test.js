a48ef5680682febb2b053849e71edde3a62a71ce
Diego Armando López <diego.lopez@activeone.co>
2020-11-15 03:34:37 -0500

    test params 4

--- backend-ppi/routes/test.js
+++ backend-ppi/routes/test.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/:path/:id", (req, res) => {
+router.get("/test/:id", (req, res) => {
 	const { path, id } = req.params;
 	res.json({ path, id });
 });
