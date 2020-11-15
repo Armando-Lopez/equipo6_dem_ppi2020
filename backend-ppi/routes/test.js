4874b0c304c0bc3e117c19da02f6fadfbe6c5361
Diego Armando López <diego.lopez@activeone.co>
2020-11-15 12:01:03 -0500

    testting post request

--- backend-ppi/routes/test.js
+++ backend-ppi/routes/test.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 router.get("/test/:id", (req, res) => {
 	const { id } = req.params;
-	res.json({ in:"test", id });
+	res.json({ in: "test", id });
+});
+
+router.post("/test", (req, res) => {
+	res.json({ data: req.body });
 });
 
 module.exports = router;
