3f242ade4e740d4b76546997e37e4ed7e3b60325
Diego Armando López <diego.lopez@activeone.co>
2020-11-15 01:03:20 -0500

    catching params

--- backend-ppi/routes/ajustes.js
+++ backend-ppi/routes/ajustes.js
@@ -6,50 +6,51 @@ const mysqlConnection = require("../db/db");
 /* Obtener la información del usuario en los 
 ajustes de la cuenta */
 router.get("/ajustes/:id", (req, res) => {
-  const { id } = req.params;
-  mysqlConnection.query(
-    `SELECT Documento, Nombre, Email, Contrasena AS 'Contraseña' FROM Usuarios WHERE Id_usuario=${id}`,
-    (err, rows, fields) => {
-      if (!err) {
-        res.json(rows);
-      } else {
-        console.log(err);
-      }
-    }
-  );
+  const { id } = req.params;  
+	res.json({ path: "Ajustes", id });
+	// mysqlConnection.query(
+	//   `SELECT Documento, Nombre, Email, Contrasena AS 'Contraseña' FROM Usuarios WHERE Id_usuario=${id}`,
+	//   (err, rows, fields) => {
+	//     if (!err) {
+	//       res.json(rows);
+	//     } else {
+	//       console.log(err);
+	//     }
+	//   }
+	// );
 });
 
 /* Actualizar la información personal del usuario
 en los ajustes de la cuenta */
 router.patch("/ajustes/:id", (req, res) => {
-  const { fecha_nacimiento, genero, ciudad_residencia } = req.body;
-  const { id } = req.params;
-  mysqlConnection.query(
-    `UPDATE Usuarios SET Fecha_de_nacimiento = ${fecha_nacimiento}, Genero = ${genero}, Ciudad= ${ciudad_residencia} WHERE Id_usuario = ${id}`,
-    (err, rows, fields) => {
-      if (!err) {
-        res.json({ status: "¡Información actualizada!" });
-      } else {
-        console.log(err);
-      }
-    }
-  );
+	const { fecha_nacimiento, genero, ciudad_residencia } = req.body;
+	const { id } = req.params;
+	mysqlConnection.query(
+		`UPDATE Usuarios SET Fecha_de_nacimiento = ${fecha_nacimiento}, Genero = ${genero}, Ciudad= ${ciudad_residencia} WHERE Id_usuario = ${id}`,
+		(err, rows, fields) => {
+			if (!err) {
+				res.json({ status: "¡Información actualizada!" });
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
 
 /* Borrar la cuenta del usuario en los ajustes de
 la cuenta */
 router.delete("/ajustes/:id", (req, res) => {
-  const { id } = req.params;
-  mysqlConnection.query(
-    `DELETE FROM Usuarios WHERE Id_usuario = ${id}`,
-    (err, rows, fields) => {
-      if (!err) {
-        res.json({ status: "¡Usuario eliminado!" });
-      } else {
-        console.log(err);
-      }
-    }
-  );
+	const { id } = req.params;
+	mysqlConnection.query(
+		`DELETE FROM Usuarios WHERE Id_usuario = ${id}`,
+		(err, rows, fields) => {
+			if (!err) {
+				res.json({ status: "¡Usuario eliminado!" });
+			} else {
+				console.log(err);
+			}
+		}
+	);
 });
 
 module.exports = router;
--- backend-ppi/src/index.js
+++ backend-ppi/src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-// const ajustes = require("../routes/ajustes");
-// const registro = require("../routes/registro");
-// const contrasena = require("../routes/contrasena");
-// const familiaviv = require("../routes/familia-y-vivienda");
+const ajustes = require("../routes/ajustes");
+const registro = require("../routes/registro");
+const contrasena = require("../routes/contrasena");
+const familiaviv = require("../routes/familia-y-vivienda");
 
 // Ajustes
 app.set("port", process.env.PORT || 3002);
@@ -14,24 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
-// Routes //
-app.use("/", (req, res) => {
-  // console.log(req);
-  
-  //configura para resivir las peticiones desde otras aplicaciones, el tipo de datos (JSON)
+//configura para resivir las peticiones desde otras aplicaciones, el tipo de datos (JSON)
+app.use((req, res, next ) => {
 	res.setHeader("Access-Control-Allow-Headers","X-Requested-With, content-type");
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE");
   res.setHeader("Content-Type", "application/json;charset=utf-8");
-  
-
-	res.json({ responde: "Hola desde el servidor :D" });
+  next();
 });
 
-// app.use("/api", ajustes);
-// app.use("/api", registro);
-// app.use("/api", contrasena);
-// app.use("/api", familiaviv);
+app.use("/api", ajustes);
+app.use("/api", registro);
+app.use("/api", contrasena);
+app.use("/api", familiaviv);
 
 // Ajustes del servidor
 app.listen(app.get("port"), () => {
