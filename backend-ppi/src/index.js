9c3561e24a2bfc2452c9b909a306669a508030b2
Diego Armando López <diego.lopez@activeone.co>
2020-11-14 22:35:12 -0500

    first api test

--- backend-ppi/src/index.js
+++ backend-ppi/src/index.js
@@ -1,23 +1,31 @@
 const express = require("express");
 const app = express();
-const ajustes = require("../routes/ajustes");
-const registro = require("../routes/registro");
-const contrasena = require("../routes/contrasena");
-const familiaviv = require("../routes/familia-y-vivienda");
+const morgan = require("morgan");
+// const ajustes = require("../routes/ajustes");
+// const registro = require("../routes/registro");
+// const contrasena = require("../routes/contrasena");
+// const familiaviv = require("../routes/familia-y-vivienda");
 
 // Ajustes
-app.set("port", 3002);
+app.set("port", process.env.PORT || 3002);
 
 // Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
 
 // Routes //
-app.use("/api", ajustes);
-app.use("/api", registro);
-app.use("/api", contrasena);
-app.use("/api", familiaviv);
+app.use("/", (req,res)=>{
+  // console.log(req);
+  res.send("Hola cucho")
+});
+
+// app.use("/api", ajustes);
+// app.use("/api", registro);
+// app.use("/api", contrasena);
+// app.use("/api", familiaviv);
 
 // Ajustes del servidor
 app.listen(app.get("port"), () => {
-  console.log(`Servidor corriendo en el puerto ${app.get("port")}`);
+	console.log(`Servidor corriendo en el puerto ${app.get("port")}`);
 });
