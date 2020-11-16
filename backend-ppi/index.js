f6abf7a8146479972e09c004fa366ff3a81d85a9
Diego Armando López <diego.lopez@activeone.co>
2020-11-15 22:23:23 -0500

    test 2

--- backend-ppi/index.js
+++ backend-ppi/index.js
@@ -18,10 +18,6 @@ app.use(morgan("dev"));
 
 //configura para resivir las peticiones desde otras aplicaciones, el tipo de datos (JSON)
 app.use((req, res, next) => {
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"X-Requested-With, content-type"
-	);
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE");
 	res.setHeader("Content-Type", "application/json;charset=utf-8");
--- backend-ppi/routes/registro.js
+++ backend-ppi/routes/registro.js
@@ -8,6 +8,10 @@ router.post("/registro", (req, res) => {
   const { documento, nombre, email, contrasena } = req.body;
   res.json({ data: req.body });
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE");
+  res.setHeader("Content-Type", "application/json;charset=utf-8");
+
   // mysqlConnection.query(
   //   `INSERT INTO Usuario(Documento, Nombre, Email, Contrasena) VALUES(${documento}, ${nombre}, ${email}, ${contrasena})`,
   //   (err, results, fields) => {
